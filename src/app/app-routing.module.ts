a43b402ad7fb31896512378b414fc66cdade05a2
angularPracticas <hugoloyolamaya@gmail.com>
2021-09-20 15:32:42 -0500

    Se agregan rutas  y la tabla en la seccion de lsta de empleados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list-empleado', pathMatch:'full'},
+  { path: 'list-empleado', component: ListEmpleadosComponent },
+  { path: 'create-empleado', component: CreateEmpleadoComponent },
+  { path: '**', redirectTo: 'list-emleado', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
